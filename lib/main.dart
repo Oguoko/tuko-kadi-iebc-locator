a5cac38f91fb953c12214f8a20890d58c27a3eca
kevdigifyke <kev.digifyke@gmail.com>
2026-04-01 09:54:24 +0000

    Your commit message

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
-import 'config/google_maps_web_loader.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await loadGoogleMapsApiForWeb();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
